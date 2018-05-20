78d1af9a37df6067362fcc04a16a3267de088e50
Diego Dariva Eltz <35403957+DDariva@users.noreply.github.com>
2018-05-20 00:01:16 -0300

    Update prova.pl

--- t4/prova.pl
+++ t4/prova.pl
@@ -1,3 +1,15 @@
+/* Uma empresa desenvolvedora de jogos precisa escolher trˆes times de colaboradores para trabalhar nos
+projetos C, D e F. Cada time ir´a trabalhar em exatamente um desses trˆes projetos. Cada time deve
+incluir ao menos um programador entre L e M e exatamente dois artistas gr´aficos entre R, S, T e
+V. Cada programador e artista gr´afico deve estar em ao menos um time, de acordo com as seguintes
+condi¸c˜oes:
+• Nenhum programador pode trabalhar em ambos os projetos C e D.
+• Nenhum artista gr´afico pode trabalhar em ambos os projetos D e F.
+• S n˜ao est´a em um time junto com T.
+• V est´a em um time junto com L.
+• V trabalha no projeto D.
+• S trabalha no projeto C. */
+
 regra1(A, B, X):-
     member(X, A),
     not(member(X, B)).
@@ -34,3 +46,12 @@ times(C, D, F):-
     regra3(C, D, F),
     member('v', D),
     member('s', C).
+
+/*Qual das seguintes alternativas ´e
+uma lista completa e correta dos times associados
+aos projetos?
+(A) C: M, V, S D: L, V, R F: M, T, R
+(B) C: M, V, R D: L, V, R F: M, S, T
+(C) C: L, V, R D: L, S, R F: M, T, V
+(D) C: L, V, S D: M, V, S F: L, T, R  CORRETA 
+(E) C: L, S, R D: M, V, T F: M, S, R */
